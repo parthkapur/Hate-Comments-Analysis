a828805b1de4234ec0a7ba63c824d9d1c46a5e37
Parth Kapur <78877957+parthkapur@users.noreply.github.com>
2021-08-30 16:09:24 +0530

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -31,7 +31,7 @@ for i in range(0,len(df)):
 #vectorization of data 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf_v=TfidfVectorizer(max_features=5000,ngram_range=(1,3))
+tfidf_v=TfidfVectorizer(max_features=80,ngram_range=(1,3))
 X=tfidf_v.fit_transform(corpus).toarray()
 y = df['class'].to_frame()
 
@@ -40,10 +40,51 @@ y = df['class'].to_frame()
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-#
+#running the model
 
 from sklearn.naive_bayes import MultinomialNB
 nvbs = MultinomialNB()
 nvbs.fit(X_train,y_train)
 nvbs_score = nvbs.score(X_test,y_test)
+print("Accuracy score for NaiveBayes is: ", nvbs_score)
 
+#from sklearn.ensemble import RandomForestClassifier
+#rfc = RandomForestClassifier()
+#rfc.fit(X_train,y_train)
+#rfc_score = rfc.score(X_test,y_test)
+#print("Accuracy score for RandomForestClassifier is: ", rfc_score)
+
+#deployment
+
+def wordopt(show):
+    show = clean(df["tweet"][i])
+    show=re.sub(r'[0-9]+', '', show) #digits
+    show=re.sub(r'[^\w\s]', '', show) #puntuation
+    show = show.lower()
+    show = show.split()
+    show = [lemmatizer.lemmatize(word) for word in show if not word in stopwords.words('english')]
+    show = ' '.join(show)
+    corpus.append(show)
+    return(show)
+
+def output_lable(n):
+    if n == 0:
+        return "Hate Speech"
+    elif n == 1:
+        return "Offensive Language"
+    elif n == 2:
+        return "Neither"
+
+vectorization = TfidfVectorizer()    
+def manual_testing(comment):
+    testing_comment = {"text":[comment]}
+    new_def_test = pd.DataFrame(testing_comment)
+    new_def_test["text"] = new_def_test["text"].apply(wordopt) 
+    new_x_test = new_def_test["text"]
+    new_xv_test = vectorization.transform(new_x_test)
+    pred_nvbs = nvbs.predict(new_xv_test)
+
+    return print("\nPrediction: {}".format(output_lable(pred_nvbs[0])))
+
+comment = str(input())
+manual_testing(comment)
